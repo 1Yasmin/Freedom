a3b4a4dfc3ff5ca40d4b54b3d700b40447128796
Yasmin Chavez <ychavez2014ig@gmail.com>
2018-04-30 16:04:27 -0600

    TODO

---
+++ Assets/vBScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class vBScript: MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject vButton;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+        vButton = GameObject.Find("actionButton");
+        
+        
+		
+	}
+
+}
