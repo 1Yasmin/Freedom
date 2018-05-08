69af040ed3556987b2379475ebcef8e09bdf7b60
Ivette <car16020@uvg.edu.gt>
2018-05-07 19:59:57 -0600

    Add

---
+++ Assets/addSwt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia; 
+
+public class addSwt : MonoBehaviour, IVirtualButtonEventHandler {
+	public GameObject gt;
+	public GameObject vbBtnObj;
+
+	// Use this for initialization
+	void Start () {
+		vbBtnObj = GameObject.Find("Add");
+		vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+		gt = GameObject.Find("Cube_001");
+		gt.gameObject.SetActive(false);
+	}
+
+	public void OnButtonPressed(VirtualButtonBehaviour vb){ 
+		Debug.Log("BTN Add pressed");
+		gt.gameObject.SetActive(true);
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb){
+		Debug.Log("Btn Add Released");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
